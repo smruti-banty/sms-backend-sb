468c00fd6ec12a1b9b75fbcd2c1d79af2a02027c
Smruti ranjan Nayak <srnayak165@gmail.com>
2022-07-05 17:05:11 +0530

    basic changes

--- src/main/java/com/smruti/demo1/SmsApplication.java
+++ src/main/java/com/smruti/demo1/SmsApplication.java
@@ -2,7 +2,6 @@ package com.smruti.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class SmsApplication {
